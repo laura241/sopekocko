ce8771c29a30bdeb6c4538fdd85bc129ed01ffde
laura241 <laura241@protonmail.com>
2020-04-20 17:30:52 +0200

    add connection to mongodb

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+mongoose
+  .connect(
+    'mongodb+srv://laura:PekockoSoProject20@cluster0-lbilb.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 const app = express()
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
 
 app.use((req, res, next) => {
-    res.json({
-        message: 'Votre requête a bien été reçue'
-    })
-    next()
+  res.json({
+    message: 'Votre requête a bien été reçue',
+  })
+  next()
 })
+
+app.use(bodyParser.json())
 
 module.exports = app
